9d55799b1a935810a8339527e4679fa060fb22c6
Joseph M Jones <the.tuxster@gmail.com>
2014-05-11 12:43:47 -0500

    change to project 1
    I've altered project 1 to use interrupts
    and 16-bit timer1

--- code-examples/Circuit_01/blink.c
+++ code-examples/Circuit_01/blink.c
@@ -1,14 +1,24 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+ISR(TIMER1_OVF_vect)
+{
+    PORTB ^= _BV(PB5);
+}
 
 int main(void)
 {
-  /* set PORTB of output */
-  DDRB |= _BV(DDB5);
+    // set PORTB PIN5 for output
+    DDRB |= _BV(DDB5);
 
-  while(1)
-  {
-    _delay_ms(50);
-    PORTB ^= _BV(PORTB5);
-  }
+    // setup timer1 with prescaler 64
+    TCCR1B |= _BV(CS10) | _BV(CS11);
+    TIMSK1 |= _BV(TOIE1);
+
+    // Enable Interupts
+    sei();
+
+
+    while(1);
 }
